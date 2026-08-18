acdc89f0fe5baf0bbb562be7d5cdb65506a8c15e
Rynard Coetzee <rcoetzee@nil.co.za>
2026-08-18 08:18:33 +0200

    Add VS

--- src/components/quickTournament/MatchCard.jsx
+++ src/components/quickTournament/MatchCard.jsx
@@ -164,7 +164,17 @@ function MatchCard({
 
                 </div>
 
-                <hr className="my-4" />
+                <div className="d-flex align-items-center my-4">
+
+                    <div className="flex-grow-1 border-top"></div>
+
+                    <span className="mx-3 text-muted fw-bold">
+                        VS
+                    </span>
+
+                    <div className="flex-grow-1 border-top"></div>
+
+                </div>
 
                 <button
                     type="button"
